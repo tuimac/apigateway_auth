0855afdf204aea7bbfdaf578562be0d8a77d9692
Ubuntu <ubuntu@docker.tuimac.private>
2021-05-06 14:30:34 +0000

    donw

--- infra/lambda.py
+++ infra/lambda.py
@@ -1,45 +1,87 @@
 #!/usr/bin/env python3
 
 import boto3
+import sys
 
-def create_user(cognito, userpoolid, clientid, username, password):
-    response = cognito.sign_up(
-        ClientId = clientid,
-        Username = username,
-        Password = password,
-        UserAttributes = [
-            {
-                'Name': 'email',
-                'Value': ''
-            }
-        ]
-    )
-    response = cognito.admin_confirm_sign_up(
-        UserPoolId = userpoolid,
-        Username = username
-    )
+USERPOOLID = ''
+CLIENTID = ''
+IDPOOLID = ''
+NAME = ''
+PASSWORD = ''
 
-def create_group(cognito, userpoolid, groupname):
-    response = cognito.create_group(
-        GroupName = groupname,
-        UserPoolId = userpoolid,
-        RoleArn = 'arn:aws:iam::002310297599:role/cognitoS3-S3AccessRole-1IM2MV67OURB5'
-    )
+def create_environment():
+    def create__iam_role():
+        policy = {
+            'Version': '2012-10-17',
+            'Statement': [
+                {
+                    'Effect': 'Allow',
+                    'Principal': {
+                        'Federated': 'cognito-identity.amazonaws.com'
+                    },
+                    'Action': 'sts:AssumeRoleWithWebIdentity',
+                    'Condition': {
+                        'StringEquals': {
+                            'cognito-identity.amazonaws.com:aud': ''
+                        },
+                        'ForAnyValue:StringLike': {
+                            "cognito-identity.amazonaws.com:amr": 'authenticated'
+                        }
+                    }
+                }
+            ]
+        }
+        policy['Statement'][0]['Condition']['StringEquals']['cognito-identity.amazonaws.com:aud'] = IDPOOLID
+        policy = json.dumps(policy)
+        iam = boto3.client('iam') 
+        response = iam.create-role(
+            RoleName = NAME + '_' + USERPOOLID,
+            AssumeRolePolicyDocument = policy,
+        )
+        return response['Role']['Arn']
 
-def add_user_to_group(cognito, userpoolid, name):
-    response = cognito.admin_add_user_to_group(
-        UserPoolId = userpoolid,
-        Username = name,
-        GroupName = name
-    )
+    def create_user(cognito):
+        response = cognito.sign_up(
+            ClientId = CLIENTID,
+            Username = NAME,
+            Password = PASSWORD,
+            UserAttributes = [
+                {
+                    'Name': 'email',
+                    'Value': ''
+                }
+            ]
+        )
+        response = cognito.admin_confirm_sign_up(
+            UserPoolId = USERPOOLID,
+            Username = NAME
+        )
+
+    def create_group(cognito, iam_role_arn):
+        response = cognito.create_group(
+            GroupName = NAME,
+            UserPoolId = USERPOOLID,
+            RoleArn = iam_role_arn
+        )
+
+    def add_user_to_group(cognito):
+        response = cognito.admin_add_user_to_group(
+            UserPoolId = USERPOOLID,
+            Username = NAME,
+            GroupName = NAME
+        )
+
+    # Initialization
+    cognito = boto3.client('cognito-idp')
+    # Create IAM Role
+    iam_role_arn = create_iam_role()
+    # Create AWS Cognito User
+    create_user(cognito)
+    # Create AWS Cognito Group
+    create_group(cognito, iam_role_arn)
+    # Make AWS Cognito User join into AWS Cognito Group
+    add_user_to_group(cognito) 
 
 if __name__ == '__main__':
-    userpoolid = ''
-    clientid = ''
-    name = ''
-    password = ''
-
-    cognito = boto3.client('cognito-idp')
-    create_user(cognito, userpoolid, clientid, name, password)
-    create_group(cognito, userpoolid, name)
-    add_user_to_group(cognito, userpoolid, name) 
+    args = sys.argv[1]
+    create_environment()
