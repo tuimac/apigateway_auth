3c368aaa3d8fb69c71808cf0bf83b17224d46341
tuimac <tuimac.devadm01@gmail.com>
2021-05-09 14:12:39 +0000

    fixed

--- script/getCognitoInfo.sh
+++ script/getCognitoInfo.sh
@@ -12,7 +12,7 @@ fi
 CLIENTID=`aws cognito-idp list-user-pool-clients --user-pool-id $USERPOOLID --max-result 20 | jq -r '.UserPoolClients[] | select(.ClientName == "cognitoS3") | .ClientId'`
 IDPID=`aws cognito-identity list-identity-pools --max-results 20 | jq -r '.IdentityPools[] | select(.IdentityPoolName == "cognitoS3") | .IdentityPoolId'`
 
-sed -i "s/export const USER_POOL_ID = \'\'/export const USER_POOL_ID = \'$USERPOOLID\'/g" $TARGETFILE
-sed -i "s/export const APP_CLIENT_ID = \'\'/export const APP_CLIENT_ID = \'$CLIENTID\'/g" $TARGETFILE
-sed -i "s/export const REGION = \'\'/export const REGION = \'$REGION\'/g" $TARGETFILE
-sed -i "s/export const ID_POOL_ID = \'\'/export const ID_POOL_ID = \'$IDPID\'/g" $TARGETFILE
+sed -i "s/export const USER_POOL_ID = \'.*\'/export const USER_POOL_ID = \'$USERPOOLID\'/g" $TARGETFILE
+sed -i "s/export const APP_CLIENT_ID = \'.*\'/export const APP_CLIENT_ID = \'$CLIENTID\'/g" $TARGETFILE
+sed -i "s/export const REGION = \'.*\'/export const REGION = \'$REGION\'/g" $TARGETFILE
+sed -i "s/export const ID_POOL_ID = \'.*\'/export const ID_POOL_ID = \'$IDPID\'/g" $TARGETFILE
