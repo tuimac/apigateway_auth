4195e956aa76b3065b6c1f139e64549c9a616247
tuimac <tuimac.devadm01@gmail.com>
2021-05-09 03:17:11 +0000

    fixed line

--- script/getCognitoInfo.sh
+++ script/getCognitoInfo.sh
@@ -3,7 +3,12 @@
 TARGETFILE='../src/src/environment.js'
 REGION='ap-northeast-1'
 
-USERPOOLID=`aws cognito-idp list-user-pools --max-results 20 | jq -r '.UserPools[] | select(.Name == "cognitoS3") | .Id'`
+cd $(dirname $0)
+USERPOOLID=`aws cognito-idp list-user-pools --max-results 20 | jq -r '.UserPools[] | select(.Name == "cognitoS3") | .Id' > /dev/null 2>&1` 
+if [ -z $USERPOOLID ]; then
+    echo 'There is no User Pool ID'
+    exit 1
+fi
 CLIENTID=`aws cognito-idp list-user-pool-clients --user-pool-id $USERPOOLID --max-result 20 | jq -r '.UserPoolClients[] | select(.ClientName == "cognitoS3") | .ClientId'`
 IDPID=`aws cognito-identity list-identity-pools --max-results 20 | jq -r '.IdentityPools[] | select(.IdentityPoolName == "cognitoS3") | .IdentityPoolId'`
 
